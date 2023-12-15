62d48886ee81f51db0bdc02f884b67227df320b9
ebanchik <ebanchik@gmail.com>
2023-12-14 23:08:59 -0500

    ad index action

--- app/views/exercises/_exercise.json.jbuilder
+++ app/views/exercises/_exercise.json.jbuilder
@@ -1,2 +1,5 @@
-json.extract! exercise, :id, :created_at, :updated_at
-json.url exercise_url(exercise, format: :json)
+json.name exercise.name
+json.sets exercise.sets
+json.reps exercise.reps
+json.break exercise.break
+json.style exercise.style
--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ExercisesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/exercises.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Exercise.count, data.length
+  end
 end
