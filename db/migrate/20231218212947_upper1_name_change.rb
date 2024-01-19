4b4d4e6a76edd107d314bd1d82f33ad1d3cc85aa
ebanchik <ebanchik@gmail.com>
2024-01-18 20:04:22 -0500

    refresh

--- db/migrate/20231218212947_upper1_name_change.rb
+++
@@ -1,5 +0,0 @@
-class Upper1NameChange < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :upper1, :upper_1_exercises
-  end
-end
--- db/migrate/20231219212950_rename_upper_1_exercises.rb
+++
@@ -1,5 +0,0 @@
-class RenameUpper1 < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :Upper1, :upper1
-  end
-end
